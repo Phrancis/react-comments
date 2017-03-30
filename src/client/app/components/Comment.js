00561e51deb8a65e9227ed32129d4eb13c9a8350
Phrancis <Phrancis@users.noreply.github.com>
2017-03-30 12:34:56 -0400

    add preventBreakOut CSS class

--- src/client/app/components/Comment.js
+++ src/client/app/components/Comment.js
@@ -2,12 +2,14 @@ import React from "react"
 import formatTimestamp from "../utils/formatTimestamp"
 
 // TODO the `comment` and `commentAuthor` CSS do not wrap long words with no spaces so they just bleed out of the HTML component
+// added `preventBreakOut` CSS class which works in some browsers, but most notably, doesn't work in Chrome
+// despite best efforts.
 const Comment = ({author, timestamp, children}) => (
-  <div className="comment">
-    <p className="commentText">
+  <div className="comment preventBreakOut">
+    <p className="commentText preventBreakOut">
       {children}
     </p>
-    <span className="commentAuthor">
+    <span className="commentAuthor preventBreakOut">
       - {author}
     </span>
     <span className="commentTimestamp">
